2069118dd92d1800282bc364f1f2d0f49e4ee867
HunterOfDreams <75785504+HunterOfDreams@users.noreply.github.com>
2026-03-14 09:20:15 +0200

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,11 @@
+function toggleStory(){
+
+const story = document.getElementById("story");
+
+if(story.style.display === "block"){
+story.style.display = "none";
+}else{
+story.style.display = "block";
+}
+
+}
